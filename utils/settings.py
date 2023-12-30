c0b30516a794c96bb3e074ba00c5d4fc111e40eb
Kamil Handzlik <kamol0209@gmail.com>
2023-12-30 17:41:05 +0100

    added initial settings for font pixels fps colors and screen size

---
+++ utils/settings.py
@@ -0,0 +1,13 @@
+import pygame as pg
+pg.init()
+
+WHITE = (255, 255, 255)         # X and O
+BLACK = (0, 0, 0)               #lines and text
+RED = (255, 0, 0)               #loser
+BLUE = (0, 255, 0)              #background color
+GREEN = (0, 0, 255)             #winner
+
+
+FPS = 60
+
+WIDTH, HEIGHT = 600, 600
