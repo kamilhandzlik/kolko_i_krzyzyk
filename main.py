e637b66b903c79b7c5359887c38b300db534fa35
Kamil Handzlik <kamol0209@gmail.com>
2024-01-02 19:19:28 +0100

    created first loop that display blank game window

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 from utils import *
+from utils.settings import *
+
+WIN = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption("Tic tac toe game")
+
+
+
+
+
+run = True
+clock = pg.time.Clock()
+
+
+while run:
+
+    clock.tick(FPS)
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            run = False
+
+pg.display.update()
--- utils/settings.py
+++ utils/settings.py
@@ -1,22 +1,8 @@
 import pygame as pg
 pg.init()
 
-WHITE = (255, 255, 255)         #lines
-BLACK = (0, 0, 0)               #text
-RED = (255, 0, 0)               # X
-BLUE = (0, 0, 255)              #background color
-GREEN = (0, 255, 0)             # O
+WIDTH, HEIGHT = 600, 600  
 
+FPS = 24
 
-FPS = 60
-
-WIDTH, HEIGHT = 600, 600
-
-ROWS = COLS = 100
-
-PIXEL_SIZE = PIXEL_SIZE = WIDTH // COLS
-
-BG_COLOR = BLUE
-
-def grt_font():
-    pg.font.SysFont("Calibri", 20)
+BG_COLOR = (0, 0, 255)
